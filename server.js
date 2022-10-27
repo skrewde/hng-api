d7edb581297b8cb545065daa6c7ad4083ff28266
skrewde <94148640+skrewde@users.noreply.github.com>
2022-10-27 23:35:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ app.get('/', (req, res) => {
 })
 const port = process.env.PORT || 5000;
 app.listen(port, () => `Server running on port ${port} 🔥`);
+
+app.get('/user', (req, res) => {
+    res.status(200).send({
+        slackUsername: 'skrewde',
+        backend:true,
+        age:22,
+        bio:'Neurodivergent programmer, INTP-t, League of Legends enjoyer.'
+    })
+});
