a81ee90157ce7c20f694ce3ce9ec1859d2732df8
skrewde <94148640+skrewde@users.noreply.github.com>
2022-10-28 10:56:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: '*'
+}));
+
 app.listen(port, () => `Server running on port ${port}`);
 
 app.get('/', (req, res) => {
